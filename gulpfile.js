dcf469a36895180d94a6491b83d11c682c59668a
snakagam <snakagam@yahoo-corp.jp>
2020-05-01 13:01:15 +0900

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
+// Task
 gulp.task('default', function () {
     return gulp.src(['./css/*.css'])
-        .pipe(autoprefixer({
-            browsers: ["last 3 versions", "ie >= 9", "Android >= 4", "ios_saf >= 8"],
-            cascade: false
-        }))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./css/gulp'));
 });
